26be49a8485738b9618fa0d5a348f6630a4dedd1
spreekaiser <thoralf.barth@gmail.com>
2023-09-05 18:19:40 +0200

    index-page: css hover functionality

--- miyawaki-forest/src/app/page.tsx
+++ miyawaki-forest/src/app/page.tsx
@@ -3,93 +3,92 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Donate a tree</h1>
-      <div className={styles.center}>
-        <Image
-          src="/images/save-planet_earth-in-hands.png"
-          alt="zwei Hände halten einen durchsichtigen Globus mit einem Baum darin"
-          width={420}
-          height={500}
-          priority
-        />
-      </div>
+    <>
+      <h1>Spende Leben</h1>
+      <main className={styles.main}>
+        <div className={styles.center}>
+          <Image
+            src="/images/save-planet_earth-in-hands.png"
+            alt="zwei Hände halten einen durchsichtigen Globus mit einem Baum darin"
+            width={420}
+            height={500}
+            priority
+          />
+        </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <div className={styles.grid}>
+          <a
+            href="#"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Unsere Projekte <span>-&gt;</span>
+            </h2>
+            <p>Lass Dich von unseren Ideen inspirieren!</p>
+          </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+          <a
+            href="#"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Baumspenden <span>-&gt;</span>
+            </h2>
+            <p>Stöber in unserer Liste guter Bäume, um zu spenden.</p>
+          </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
+          <a
+            href="#"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Zusammenhalt <span>-&gt;</span>
+            </h2>
+            <p>Schließ Dich uns an! Nur zusammen sind wir stark.</p>
+          </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-      <div className={styles.navigation}>
-        <a href="">
-          <div>
-            <Image
-              src="/images/logo-1_project-3.png"
-              alt="Planet wird mit einem Ast umrundet"
-              width={75}
-              height={60}
-              priority
-            />
-          </div>
-        </a>
-        <a href="">
-          <div>
-            <Image
-              src="/images/logo-1_donation-1.png"
-              alt="zwei Hände aus denen ein Baum wächst"
-              width={70}
-              height={60}
-              priority
-            />
-          </div>
-        </a>
-        {/* <div>
+          <a
+            href="#"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Wissenswertes <span>-&gt;</span>
+            </h2>
+            <p>Erfahre mehr über aktivien Naturschutz.</p>
+          </a>
+        </div>
+        <div className={styles.navigation}>
+          <a href="">
+            <div>
+              <Image
+                src="/images/logo-1_project-4.png"
+                alt="Das drei Pfeile Recycling-Symbol. Ein Pfeil ist durch ein Blatt ersetzt"
+                width={70}
+                height={60}
+                priority
+              />
+            </div>
+          </a>
+          <a href="">
+            <div>
+              <Image
+                src="/images/logo-1_donation-1.png"
+                alt="zwei Hände aus denen ein Baum wächst"
+                width={70}
+                height={60}
+                priority
+              />
+            </div>
+          </a>
+          {/* <div>
           <Image
             src="/images/logo-1_comunity.png"
             alt="drei Personen abstrakt als Bäume dargestellt. Mit erhobenen Armen aud denen Blätter wachsen"
@@ -98,29 +97,30 @@ export default function Home() {
             priority
           />
         </div> */}
-        <a href="">
-          <div>
-            <Image
-              src="/images/logo-1_user-2.png"
-              alt="Person die von einem abstrakten Ast und einem Wasserkreis umrundet wird. Am Ende des Astes wachsen Blätter"
-              width={65}
-              height={60}
-              priority
-            />
-          </div>
-        </a>
-        <a href="">
-          <div>
-            <Image
-              src="/images/logo-1_blog-1.png"
-              alt="Das drei Pfeile Recycling-Symbol. Ein Pfeil ist durch ein Blatt ersetzt"
-              width={64}
-              height={60}
-              priority
-            />
-          </div>
-        </a>
-      </div>
-    </main>
+          <a href="">
+            <div>
+              <Image
+                src="/images/logo-1_user-2.png"
+                alt="Person die von einem abstrakten Ast und einem Wasserkreis umrundet wird. Am Ende des Astes wachsen Blätter"
+                width={65}
+                height={60}
+                priority
+              />
+            </div>
+          </a>
+          <a href="">
+            <div>
+              <Image
+                src="/images/logo-1_blog-1.png"
+                alt="Aus einem aufgeschlagenem Buch wächst ein grüne Stadt mit einem zentralen Baum"
+                width={64}
+                height={60}
+                priority
+              />
+            </div>
+          </a>
+        </div>
+      </main>
+    </>
   );
 }
